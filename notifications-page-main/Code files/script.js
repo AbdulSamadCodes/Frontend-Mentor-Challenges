9cb965259dbaae3b40a4ca76ff7ae50fba62a78e
Abdul Samad <samadcom797@gmail.com>
2024-02-09 23:05:42 +0500

    Notification page completed

---
+++ notifications-page-main/Code files/script.js
@@ -0,0 +1,43 @@
+"use strict"
+
+
+//-----Notifications mark read functionality---------//
+
+const markReadButton = document.querySelector("[data-markread]");
+const openNotifications = document.querySelectorAll("[data-open-notification]");
+const markers = document.querySelectorAll("[data-marker]");
+
+markReadButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    for (let i = 0; i < openNotifications.length; i++) {
+        openNotifications[i].style.backgroundColor = "#fff";
+        markers[i].style.display = "none";
+    }
+})
+
+
+
+//-----Functionality  to toggle Privatemeesage---------//
+
+const privateMessage = document.querySelector("[data-privatemessage]");
+const messageToggler = document.querySelector("[data-message-toggler]");
+
+
+messageToggler.addEventListener("click", (event) => {
+    event.preventDefault();
+    privateMessage.classList.toggle("active");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
